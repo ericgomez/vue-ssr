6710d8c01c2360964b6c36f4abef203dfa9ef45b
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-11-30 12:50:55 -0600

    Rutas del perfil de usuario con NodeJS

--- src-ssr/app/routes/profile.js
+++ src-ssr/app/routes/profile.js
@@ -1,3 +1,26 @@
-module.exports = (app) => {
+const User = require('../models').User;
+const bcrypt = require('bcrypt');
+const saltRounds = require('./../config').saltRounds;
 
+// app extended of index.js in src-ssr
+// app contain modelude of express
+module.exports = (app) => {
+  app.patch('/api/profile/updates', async (req, res) => {
+    // function fetch is method of bookshelf
+    const user = await new User({ 'id': req.session.userID }).fetch();
+
+    if (user) {
+      const hash = await bcrypt.hash(req.body.password, saltRounds);
+      await user.save({ name: req.body.name, password: hash }, { patch: true });
+
+      req.session.name = req.body.name;
+
+      return res.json({
+        res: 'ok',
+        message: 'Perfil actualizado correctamente',
+        name: req.body.name
+      })
+        .status(200);
+    }
+  });
 };
