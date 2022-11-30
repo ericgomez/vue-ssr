0d77f7a94d7ea4f3633c40c931be2f4ea0bc6d59
Eric Gomez <eric.gomez.sanchez@gmail.com>
2022-11-29 19:20:36 -0600

    Crear el archivo de configuración de Knex (Migraciones DB)

---
+++ src-ssr/knexfile.js
@@ -0,0 +1,23 @@
+require('dotenv').config({ path: '../.env' });
+
+module.exports = {
+  // entorno de desarrollo: development
+  development: {
+    client: 'mysql',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
+      charset: process.env.DB_CHARSET
+    },
+    // creamos un directorio de nombre: db donde se iran creando los seeds y migrations
+    migrations: {
+      directory: './db/migrations',
+      tableName: 'knex_migrations'
+    },
+    seeds: {
+      directory: './db/seeds',
+    }
+  }
+};
